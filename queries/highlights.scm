cdf2a90256aa881d9570c9647406ead94dde8fee
Tnze <cjd001113@outlook.com>
2024-06-14 01:16:06 +0800

    Add highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,14 @@
+(pair
+    key: (_) @string.special.key)
+
+(string) @string
+
+(byte) @number.byte
+(short) @number.short
+(int) @number.int
+(long) @number.long
+
+(float) @number.float
+(double) @number.double
+
+(escape_sequence) @escape
